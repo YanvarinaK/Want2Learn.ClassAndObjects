88711ef28ddd9f17647e483afe3edc7728ca4231
Yana <yankayanka4815@gmail.com>
2021-07-30 22:55:04 +0300

    Create class User

---
+++ Want2Learn.ClassAndObjects/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Want2Learn.ClassAndObjects
+{
+    public class User
+    {
+        private string login;
+        private string email;
+        private string password;
+        private string fullName;
+        private bool isDeleted;
+    }
+}
