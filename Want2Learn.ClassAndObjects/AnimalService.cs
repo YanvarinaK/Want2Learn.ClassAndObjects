1c7592b3d07e048364700a4bfe7ff5905026eac3
Yana <yankayanka4815@gmail.com>
2021-08-14 20:55:47 +0300

    Create Animalservice: functions to write list\array in json format

---
+++ Want2Learn.ClassAndObjects/AnimalService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Want2Learn.ClassAndObjects
+{
+    public class AnimalService
+    {
+        public void WriteInfo(List<Animal> animals, string filePath)
+        {
+            if ((animals == null) || (filePath == null))
+            {
+                return;
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+            {
+                streamWriter.WriteLine(JsonConvert.SerializeObject(animals, Formatting.Indented));
+            }
+        }
+
+        public void WriteInfo(Animal[] animals, string filePath)
+        {
+            if ((animals == null) || (filePath == null))
+            {
+                return;
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+            {
+                streamWriter.WriteLine(JsonConvert.SerializeObject(animals, Formatting.Indented));
+            }
+        }
+    }
+}
