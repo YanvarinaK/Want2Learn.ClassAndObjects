5aefd8277cb98a9d0d423684ef75a851631ea6cf
Yana <yankayanka4815@gmail.com>
2021-08-14 16:25:53 +0300

    Create classes: Animal, Dog, Cat

---
+++ Want2Learn.ClassAndObjects/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Want2Learn.ClassAndObjects
+{
+    public class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+        public override string GetInfo()
+        {
+            string info = String.Format("Name: {0}, Color: {1}, Breed: {2}", Name, AnimalColor, Breed);
+            return info;
+        }
+    }
+}
