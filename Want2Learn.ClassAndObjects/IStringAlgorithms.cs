f089653904c86b8c487cc096ce81b57097ecf2bd
Yana <yankayanka4815@gmail.com>
2021-08-24 18:38:39 +0300

    Create Vigenere encryption/decryption

---
+++ Want2Learn.ClassAndObjects/IStringAlgorithms.cs
@@ -0,0 +1,8 @@
+﻿namespace Want2Learn.ClassAndObjects
+{
+    public interface IStringAlgorithms
+    {
+        string VigenereDecryption(string text, string key);
+        string VigenereEncryption(string text, string key);
+    }
+}
---
+++ Want2Learn.ClassAndObjects/StringAlgorithms.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Want2Learn.ClassAndObjects
+{
+    public class StringAlgorithms : IStringAlgorithms
+    {
+        private string GetStringOfKeys(string text, string key)
+        {
+            string StringOfKeys = String.Join("", Enumerable.Repeat(key, text.Length / key.Length));
+            for (int i = 0; i < (text.Length % key.Length); i++)
+            {
+                StringOfKeys = StringOfKeys + key[i];
+            }
+            return StringOfKeys;
+        }
+        public string VigenereEncryption(string text, string key)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+            if (String.IsNullOrEmpty(key))
+            {
+                return text;
+            }
+
+            string StringOfKeys = GetStringOfKeys(text, key);
+            string Encryption = String.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Encryption = Encryption + (char)(text[i] + StringOfKeys[i]);
+            }
+            return Encryption;
+        }
+
+        public string VigenereDecryption(string text, string key)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+            if (String.IsNullOrEmpty(key))
+            {
+                return text;
+            }
+            string StringOfKeys = GetStringOfKeys(text, key);
+            string Decryption = String.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Decryption = Decryption + (char)(text[i] - StringOfKeys[i]);
+            }
+            return Decryption;
+        }
+    }
+}
